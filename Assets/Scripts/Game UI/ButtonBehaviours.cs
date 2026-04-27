d801e2880662acf6315751918971b35ea7bdc269
redmartian-droid <118920978+redmartian-droid@users.noreply.github.com>
2026-04-27 02:16:30 +0200

    Main menu UI

--- Assets/Scripts/Game UI/ButtonBehaviours.cs
+++ Assets/Scripts/Game UI/ButtonBehaviours.cs
@@ -5,6 +5,7 @@ public class ButtonBehaviours : MonoBehaviour, IPointerEnterHandler
 {
     public void OnPointerEnter(PointerEventData eventData)
     {
-        AudioManager.Instance.PlaySound("ButtonHover");
+        AudioManager.Instance.PlaySound("ButtonHover");      
     }
+
 }
