ed2adfa1628ee94660188b1de6acb098ea6920d7
kokostahr <starking.nn@gmail.com>
2026-06-10 23:34:15 +0200

    created pause menu script and pause input action
    time to do the logic

---
+++ Assets/Scripts/Game UI/PauseMenu.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    //this script is gonna reference the wizardcontroller script to enable /disable the cursor lock state. idk how that makes sense
+    //but here we go lol. 
+    //want the script to only pause for the local player (these auto comments are SO ANNOYING)
+
+
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
