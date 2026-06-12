ae4a3578ff2c6bd3dd08e88d93835ad26065ff48
mikisha-ap <mikisha.aryanna@gmail.com>
2026-06-13 00:16:37 +0200

    fixing hotbar stuff

--- Assets/Scripts/Game UI/SessionCutscenes.cs
+++ Assets/Scripts/Game UI/SessionCutscenes.cs
@@ -1,87 +1,64 @@
 using System.Collections;
-using UnityEngine;
-using UnityEngine.Video;
-using UnityEngine.SceneManagement;
 using Unity.Netcode;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.Video;
 
-public class SessionCutscenes : NetworkBehaviour
+public class SessionCutscenes : NetworkBehaviour // changed to network
 {
-    [SerializeField] private GameObject introPanel;
-    [SerializeField] private GameObject winPanel;
-    [SerializeField] private GameObject losePanel;
+	[SerializeField] private GameObject introPanel;
+	[SerializeField] private GameObject winPanel;
+	[SerializeField] private GameObject losePanel;
 
-    [SerializeField] private GameObject puzzle1CompletePanel;
-    [SerializeField] private GameObject bothCutscenePanel;
+	[SerializeField] private GameObject puzzle1CompletePanel;
+	[SerializeField] private GameObject bothCutscenePanel;
 
-    public InteractionHandler interactionHandler;
+	public UnityEvent bothCutsceneCompleteEvent;
 
-    private bool _isFirstScene = true;
+	private void Start()
+	{
+		StartCoroutine(PlayClip(introPanel, onComplete: () => introPanel.SetActive(false)));
+	}
 
-    private void Start()
-    {
-        StartCoroutine(PlayClip(introPanel, onComplete: () => introPanel.SetActive(false)));
-    }
+	[ClientRpc]
+	public void TriggerWinClientRpc() => StartCoroutine(PlayClip(winPanel, onComplete: LoadMainMenu)); // changed to client rpc for both
 
-    private void OnEnable()
-    {
-        SceneManager.sceneLoaded += OnSceneLoaded;
-    }
+	[ClientRpc]
+	public void TriggerLoseClientRpc() => StartCoroutine(PlayClip(losePanel, onComplete: LoadMainMenu));
 
-    private void OnDisable()
-    {
-        SceneManager.sceneLoaded -= OnSceneLoaded;
-    }
+	public void TriggerPuzzle1Complete()
+	{
+		StartCoroutine(PlayClip(puzzle1CompletePanel, onComplete: () => puzzle1CompletePanel.SetActive(false)));
+	}
 
-    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
-    {
-        if (_isFirstScene)
-        {
-            _isFirstScene = false;
-            return;
-        }
+	public void TriggerBothCutscene()
+	{
+		StartCoroutine(PlayClip(bothCutscenePanel, onComplete: LoadMainMenu));
+	}
 
-        interactionHandler.Initialize();
-        StartCoroutine(PlayClip(introPanel, onComplete: () => introPanel.SetActive(false)));
-    }
 
-    [ClientRpc]
-    public void TriggerWinClientRpc() => StartCoroutine(PlayClip(winPanel, onComplete: LoadMainMenu));
+	private IEnumerator PlayClip(GameObject panel, System.Action onComplete)
+	{
+		panel.SetActive(true);
 
-    [ClientRpc]
-    public void TriggerLoseClientRpc() => StartCoroutine(PlayClip(losePanel, onComplete: LoadMainMenu));
+		MusicManager.Instance.SetPaused(true); // stop music
 
-    public void TriggerPuzzle1Complete()
-    {
-        StartCoroutine(PlayClip(puzzle1CompletePanel, onComplete: () => puzzle1CompletePanel.SetActive(false)));
-    }
+		VideoPlayer video = panel.GetComponent<VideoPlayer>();
 
-    public void TriggerBothCutscene()
-    {
-        StartCoroutine(PlayClip(bothCutscenePanel, onComplete: LoadMainMenu));
-    }
+		video.Prepare();
+		yield return new WaitUntil(() => video.isPrepared);
 
-    private IEnumerator PlayClip(GameObject panel, System.Action onComplete)
-    {
-        panel.SetActive(true);
+		video.Play();
+		yield return null; // one frame for isPlaying to become true
+		yield return new WaitUntil(() => !video.isPlaying);
 
-        MusicManager.Instance.SetPaused(true);
+		MusicManager.Instance.SetPaused(false); // resume music
 
-        VideoPlayer video = panel.GetComponent<VideoPlayer>();
+		onComplete?.Invoke();
+	}
 
-        video.Prepare();
-        yield return new WaitUntil(() => video.isPrepared);
-
-        video.Play();
-        yield return null;
-        yield return new WaitUntil(() => !video.isPlaying);
-
-        MusicManager.Instance.SetPaused(false);
-
-        onComplete?.Invoke();
-    }
-
-    private void LoadMainMenu()
-    {
-        NetworkManager.Singleton.SceneManager.LoadScene("Main Menu", LoadSceneMode.Single);
-    }
+	private void LoadMainMenu()
+	{
+		NetworkManager.Singleton.SceneManager.LoadScene("Main Menu", UnityEngine.SceneManagement.LoadSceneMode.Single);
+	}
 }
