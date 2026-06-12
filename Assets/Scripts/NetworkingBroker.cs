d59e8f08b64e1e943437fa318c11c3ae8a739ee4
YoavLippi <2567169@students.wits.ac.za>
2026-06-12 11:48:29 +0200

    Added missing components from previous commit

--- Assets/Scripts/NetworkingBroker.cs
+++ Assets/Scripts/NetworkingBroker.cs
@@ -1,5 +1,6 @@
 using Unity.Netcode;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class NetworkingBroker : NetworkBehaviour
 {
@@ -22,5 +23,11 @@ public class NetworkingBroker : NetworkBehaviour
     private void SendMessageGlobalRpc(string message)
     {
         NetworkManager.Singleton.LocalClient.PlayerObject.SendMessage(message);
+    }
+
+    [Rpc(SendTo.Everyone)]
+    public void LoadSceneRpc(string sceneName)
+    {
+        NetworkManager.Singleton.SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
     }
 }
